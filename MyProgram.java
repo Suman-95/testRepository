3e9cb41e3eda07bd0e4f6425ff07922342ea88f5
Suman-95 <sumanrajnayak@gmail.com>
2020-08-24 23:47:22 +0530

    Commiting To check Jenkins

--- MyProgram.java
+++ MyProgram.java
@@ -1,6 +1,6 @@
 class MyProgram{
 public static void main(String[] args){
-for(int i=0;i<19;i++){
-	System.out.println("Running time:"+i);
+for(int i=0;i<15;i++){
+	System.out.println("Running time is:"+i);
 }
 }}
